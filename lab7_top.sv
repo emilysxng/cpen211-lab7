2cfc83b588ded55819f3d7243ca625b2bae50996
Emily Song <song.emily2004@gmail.com>
2023-11-26 21:59:21 -0800

    aslkdfjklsf

--- lab7_top.sv
+++ lab7_top.sv
@@ -1,15 +1,27 @@
+`define MREAD 2'b10
+`define MWRITE 2'b01
+`define MNONE 2'b00
+
 module lab7_top(KEY,SW,LEDR,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
     input [3:0] KEY;
     input [9:0] SW;
     output [9:0] LEDR;
     output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+    wire msel;
+    wire and_result;
+    wire dout;
 
     // The memory is initialized with instructions contained in a file data.txt. The
     // format of each line in data.txt is “@<addr> <contents>”.
 
-    RAM MEM(clk,read_address,write_address,write,din,dout);
+    RAM MEM(clk, mem_addr, write_address, write, din, dout); //write_address, write, din still need to be instantiated (stage 3)
 
-    
+    assign msel = (mem_addr = 1'b0) ? 1'b1 : 1'b0;
+
+    assign and_result = ((mem_addr = 1'b0) ? 1'b1 : 1'b0) & msel;
+
+    //tri-state driver
+    assign dout = and_result ? read_data : {16{1’bz}};
 endmodule
 
 module RAM(clk,read_address,write_address,write,din,dout);
